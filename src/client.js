97b5fa19cc0fd54f13b48248c92ba26351e6bbc6
Yurii Rybachok <drummer2009@ukr.net>
2023-05-13 15:24:35 +0300

    add sanity to project

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 const sanityClient = require('@sanity/client')
 const client = sanityClient({
-    projectId: '0hyvysvv',
+    projectId: 'tsnlsscs',
     dataset: 'production',
     apiVersion: '2021-03-25',
     useCdn: true,
